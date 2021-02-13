61ae99a5e30652f1c3ab312596634d0c9c20d8ab
gitthitat laosing <gitth12345@gmail.com>
2021-02-13 14:28:12 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="/">หน้าแรก</a> | <a href="/about">เกี่ยวกับเรา</a> | <a href="/contact">ติดต่อเรา</a>
+@extends('layout.master')
+
+@section('content')
+
 <h1>Hello world</h1>
-</body>
-</html>
+
+@endsection
